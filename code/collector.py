258c728e7e683504efd9de792055561406ab5001
mcbyte_it <rkattan@gmail.com>
2025-03-10 19:36:24 +0200

    updates to a new version of the code

--- code/collector.py
+++ code/collector.py
@@ -1,147 +1,19 @@
-import base64
-import logging
 import os
-import time
+import random
 
 import requests
 from Crypto.Cipher import AES
-from prometheus_client import start_http_server
-from prometheus_client.core import GaugeMetricFamily, REGISTRY, CounterMetricFamily
+from flask import Flask, Response
+from prometheus_client import Gauge, Counter, generate_latest
 
-__version__ = "develop"
-
-# username to access thw webui
-USERNAME = ''
-# username to access thw webui
-PASSWORD = ''
-# URL for the webUI including the port
-HOSTNAME = 'http://192.168.1.1'
-
-ch = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s %(levelname)8s %(name)s | %(message)s")
-ch.setFormatter(formatter)
-
-logger = logging.getLogger("fiberhome_logger")
-logger.addHandler(ch)
-
-log_level = "INFO"
-
-loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
-for log in loggers:
-    log.setLevel(log_level)
-
-
-class FiberHomeCollector(object):
-    def __init__(self):
-        pass
-
-    def collect(self):
-        base_url = HOSTNAME
-        ajax_endpoint = base_url + "/cgi-bin/ajax?ajaxmethod="
-        login_url = base_url + "/cgi-bin/ajax"
-
-        resp = requests.get(url=ajax_endpoint + "get_base_info")
-        data = resp.json()  # Check the JSON Response Content documentation below
-        valid_session = data['session_valid']
-        logger.info("valid session?")
-        logger.info(valid_session)
-
-        if valid_session:
-            pass
-            # logger.info("valid session")
-        else:
-            logger.info("Not logged in, need to pass data")
-
-            resp = requests.get(url=ajax_endpoint + "get_refresh_sessionid")
-            data = resp.json()  # Check the JSON Response Content documentation below
-
-            new_sessionid = data['sessionid']
-            logger.info("new sessionid: " + new_sessionid)
-
-            post_data = {"username": USERNAME,
-                         "loginpd": fhencrypt(PASSWORD),
-                         "port": "0",
-                         "sessionid": new_sessionid,
-                         "ajaxmethod": "do_login"}
-
-            resp = requests.post(url=login_url, data=post_data)
-            # logger.info("post data: " + resp.text)
-
-            data = resp.json()
-            login_session_valid = data['session_valid']
-
-            if login_session_valid:
-                logger.info("We are now logged in")
-                pass
-            else:
-                logger.error("Something went wrong, please check the credentials and try again")
-                exit(1)
-
-        resp = requests.get(url=ajax_endpoint + "get_base_info")
-        data = resp.json()
-
-        # logger.info("uptime: " + data['uptime'])
-
-        # array starts at index 0
-        base_metrics = [
-            ['c', 'uptime_seconds_total', 'Uptime in seconds', ['ModelName', 'SerialNumber', 'SoftwareVersion'],
-             'seconds', 'uptime'],
-            ['g', 'memory_total_bytes', 'Total memory', ['ModelName', 'SerialNumber', 'SoftwareVersion'],
-             'bytes', 'mem_total'],
-            ['g', 'memory_free_bytes', 'Free memory', ['ModelName', 'SerialNumber', 'SoftwareVersion'],
-             'bytes', 'mem_free'],
-            ['g', 'cpu_usage', 'CPU usage', ['ModelName', 'SerialNumber', 'SoftwareVersion'],
-             '', 'cpu_usage'],
-            ['c', 'pon_bytes_total', 'PON bytes sent', ['ModelName', 'SerialNumber', 'SoftwareVersion'],
-             'bytes', 'ponBytesSent', 'tx'],
-            ['c', 'pon_bytes_total', 'PON  bytes received', ['ModelName', 'SerialNumber', 'SoftwareVersion'],
-             'bytes', 'ponBytesReceived', 'rx'],
-            ['c', 'pon_packets_total', 'PON packets sent', ['ModelName', 'SerialNumber', 'SoftwareVersion'],
-             'packets', 'ponPacketsSent', 'tx'],
-            ['c', 'pon_packets_total', 'PON  packets received', ['ModelName', 'SerialNumber', 'SoftwareVersion'],
-             'packets', 'ponPacketsReceived', 'rx'],
-            ['g', 'power', 'Power in transmission', ['ModelName', 'SerialNumber', 'SoftwareVersion'],
-             '', 'txpower', 'tx'],
-            ['g', 'power', 'Power in reception', ['ModelName', 'SerialNumber', 'SoftwareVersion'],
-             '', 'rxpower', 'rx'],
-            ['g', 'transceiver_temperature', 'Transceiver temperature', ['ModelName', 'SerialNumber', 'SoftwareVersion'],
-             '', 'transceivertemperature'],
-        ]
-
-        prom_metrics = []
-        idx = 0
-        for metric in base_metrics:
-            label_names = []
-            for value in metric[3]:
-                label_names.append(value.lower())
-            try:
-                if metric[6]:
-                    label_names.append('direction')
-            except IndexError:
-                pass
-
-            if metric[0] == "c":
-                mtr = CounterMetricFamily("fiberhome_" + metric[1], metric[2], labels=label_names, unit=metric[4])
-            elif metric[0] == "g":
-                mtr = GaugeMetricFamily("fiberhome_" + metric[1], metric[2], labels=label_names, unit=metric[4])
-
-            label_values = []
-            for value in metric[3]:
-                label_values.append(data[value])
-            try:
-                if metric[6]:
-                    label_values.append(metric[6])
-            except IndexError:
-                pass
-            mtr.add_metric(labels=label_values, value=data[metric[5]])
-            yield mtr
-            prom_metrics.append(mtr)
-
-        # $base_info = curlGetJson("get_base_info"); // NEEDED - more status of above, used lan ports, serial, modem,
-        # $wan_info = curlGetJson("get_allwan_info"); // WAN info,
-        # $lan_info = curlGetJson("get_ipv4_lan_info"); // LAN ipv4 info,
-        # $ethernet_info = curlGetJson("getEthernetPorts_info"); // LAN ports info, connected and total packets
-
+# Configuration
+ROUTER_IP = os.getenv("ROUTER_IP", "http://192.168.1.1")
+# AJAX_ENDPOINT = f"{ROUTER_IP}/cgi-bin/ajax?ajaxmethod="
+STATUS_URL = f"{ROUTER_IP}/cgi-bin/ajax?ajaxmethod=get_base_info&_="
+SESSION_ID_URL = f"{ROUTER_IP}/cgi-bin/ajax?ajaxmethod=get_refresh_sessionid"
+LOGIN_URL = f"{ROUTER_IP}/cgi-bin/ajax"
+USERNAME = os.getenv("ROUTER_USERNAME", "admin")
+PASSWORD = os.getenv("ROUTER_PASSWORD", "admin1234")
 
 def pkcs7padding(data, block_size=16):
     if type(data) != bytearray and type(data) != bytes:
@@ -175,29 +47,96 @@ def fhdecrypt(data):
     return decrypted_pass.strip().decode('utf8')
 
 
-if __name__ == '__main__':
-    # get username from ENV, defaults to 'admin'
-    USERNAME = os.getenv('USERNAME', 'admin')
+# Prometheus metrics
+uptime = Gauge("router_uptime", "Router uptime in seconds")
+mem_total = Gauge("router_mem_total", "Total memory in KB")
+mem_free = Gauge("router_mem_free", "Free memory in KB")
+cpu_usage = Gauge("router_cpu_usage", "CPU usage percentage")
+pon_bytes_sent = Counter("router_pon_bytes_sent", "PON bytes sent")
+pon_bytes_received = Counter("router_pon_bytes_received", "PON bytes received")
+pon_packets_sent = Counter("router_pon_packets_sent", "PON packets sent")
+pon_packets_received = Counter("router_pon_packets_received", "PON packets received")
+tx_power = Gauge("router_tx_power", "TX power level")
+rx_power = Gauge("router_rx_power", "RX power level")
+transceiver_temperature = Gauge("router_transceiver_temperature", "Transceiver temperature in Celsius")
 
-    # get username from ENV
-    PASSWORD = os.getenv('PASSWORD')
+# Flask app
+app = Flask(__name__)
 
-    # get hostname and port from ENV, defaults to http://192.168.1.1
-    HOSTNAME = os.getenv('HOSTNAME', 'http://192.168.1.1')
+def get_router_data():
+    response = requests.get(f"{STATUS_URL}{random.random()}")
 
-    # logger.info("username: " + USERNAME)
-    # logger.info("password: " + PASSWORD)
-    # logger.info("hostname: " + HOSTNAME)
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        if not data.get("session_valid", False):  # Check session validity from JSON response
+            session_id_response = requests.get(SESSION_ID_URL)
+            print(session_id_response)
+            if session_id_response.status_code != 200:
+                print("Failed to retrieve session ID")
+                return None
+            session_id_data = session_id_response.json()
+            new_sessionid = session_id_data.get("sessionid", "")
 
-    if USERNAME is None or PASSWORD is None or HOSTNAME == "":
-        logger.error("Credentials not found or not correct, please check the Environmental variables")
-        logger.error("USERNAME=<username>")
-        logger.error("PASSWORD=<password>")
-        logger.error("HOSTNAME=<url of the router. OPTIONAL, defaults to http://192.168.1.1>")
-        exit(1)
+            login_payload = {"username": USERNAME,
+                         "loginpd": fhencrypt(PASSWORD),
+                         "port": "0",
+                         "sessionid": new_sessionid,
+                         "ajaxmethod": "do_login",
+                         "_": f'{random.random()}'}
+            print(login_payload)
+            login_response = requests.post(LOGIN_URL, data=login_payload)
+            if login_response.status_code != 200:
+                print("Failed to log in to the router")
+                return None
+            login_data = login_response.json()
+            print(login_data)
+            if login_data.get("login_result") == 2:
+                print("Error: Username or password is wrong 3 times, please retry 1 minute later! ")
+                return None
+            if login_data.get("login_result") != 0:
+                print(f"Login failed with result code: {login_data.get('login_result')}")
+                return None
 
-    logger.info("Starting collector on port 6145")
-    start_http_server(6145)
-    REGISTRY.register(FiberHomeCollector())
-    while True:
-        time.sleep(5)
+            if not login_data.get("session_valid", False):  # Check session validity after login
+                print("Session still not valid after login")
+                return None
+            response = requests.get(f"{STATUS_URL}{random.random()}")
+            if response.status_code == 200:
+                data = response.json()
+            else:
+                print("Failed to retrieve data after login")
+                return None
+        return data
+    else:
+        print("Failed to retrieve data")
+        return None
+
+def update_metrics():
+    data = get_router_data()
+    print(data)
+    if data:
+        #download_speed.set(data.get("download", 0))
+        #upload_speed.set(data.get("upload", 0))
+        uptime.set((data.get("uptime", 0)))
+        mem_total.set((data.get("mem_total", 0)))
+        mem_free.set((data.get("mem_free", 0)))
+        cpu_usage.set((data.get("cpu_usage", 0)))
+        pon_bytes_sent.inc(int(data.get("ponBytesSent", 0)))
+        pon_bytes_received.inc(int(data.get("ponBytesReceived", 0)))
+        pon_packets_sent.inc(int(data.get("ponPacketsSent", 0)))
+        pon_packets_received.inc(int(data.get("ponPacketsReceived", 0)))
+        tx_power.set((data.get("txpower", 0)))
+        rx_power.set((data.get("rxpower", 0)))
+        transceiver_temperature.set(data.get("transceivertemperature", 0))
+
+def generate_metrics():
+    update_metrics()
+    return generate_latest()
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_metrics(), mimetype="text/plain")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=6145)
